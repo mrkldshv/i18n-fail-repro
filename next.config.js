f363b81f06e3dff3578cddbdd5753e6aa7871c07
Mark <mark.ladyshau@olx.pl>
2022-11-13 14:28:27 +0100

    Change config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  experimental: {
+    runtime: "experimental-edge",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
